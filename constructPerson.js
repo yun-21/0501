3661db45409b31d98ec0dd501c28e2b9bcb3845a
jiyun <ranji12@naver.com>
2024-06-16 20:59:17 +0900

    생성자 함수에서 정의한 construtor(생성자), 프로토타입 메서드, 정적 메서드

---
+++ constructPerson.js
@@ -0,0 +1,16 @@
+var Person = (function(){
+    //생성자 함수
+    function Person(name){
+        this.name = name;
+    }
+    //프로토타입 메서드
+    Person.prototype.sayHi = function(){
+        console.log('Hi, My name is '+ this.name);
+    }
+    //정적 메서드
+    Person.sayHello = function (){
+        console.log('Hello!');
+    }
+    //생성자 함수 반환
+    return Person;
+}());
